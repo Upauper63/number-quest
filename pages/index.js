ba15b10d708171fb8f9d198075dce05c5873cc58
Upauper63 <upauper63@gmail.com>
2023-07-04 13:42:08 +0900

    add prototype fix

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,27 @@
-import styles from '../styles/Home.module.css'
-import { Button, Grid, Typography } from '@mui/material'
-import Link from 'next/link'
+import styles from "../styles/Home.module.css";
+import { Button, Grid, Typography } from "@mui/material";
+import Link from "next/link";
 
 export default function Home() {
   return (
-<Grid container alignItems={'center'} justifyContent={'center'} direction={'column'} mt={5} spacing={5}>
-
-      <Grid><Typography variant='h2'>□ を求める計算の練習！</Typography></Grid>
-      <Grid mt={5}>
-      <Link href="add" passHref>
-        <Button variant="contained">足し算</Button>
-      </Link>
+    <Grid
+      container
+      alignItems={"center"}
+      justifyContent={"center"}
+      direction={"column"}
+      mt={5}
+      spacing={5}
+    >
+      <Grid item>
+        <Typography variant="h4" textAlign={"center"}>
+          □ の計算練習！
+        </Typography>
       </Grid>
+      <Grid item mt={2}>
+        <Link href="add" passHref>
+          <Button variant="contained">足し算</Button>
+        </Link>
       </Grid>
-  )
+    </Grid>
+  );
 }
