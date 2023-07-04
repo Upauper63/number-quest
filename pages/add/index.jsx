02f1449249c87500191fd5a0bd4d019b268c038c
Upauper63 <upauper63@gmail.com>
2023-07-04 10:28:33 +0900

    input type test

---
+++ pages/add/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const index = () => {
+    
+  return (
+    <div>
+      <div>q</div>
+      <div>□ = <input type="number" /></div>
+
+    </div>
+  )
+}
+
+export default index
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Button } from '@mui/material'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-first commit
+      <h1>□ を求める計算の練習！</h1>
+      <Link href="add" passHref>
+        <Button variant="contained">足し算</Button>
+      </Link>
     </div>
   )
 }
