bda0c86a042b4e160160e578d8f8aeaff6aeed6e
Upauper63 <upauper63@gmail.com>
2023-07-04 11:08:19 +0900

    input type test fix2

--- pages/add/index.jsx
+++ pages/add/index.jsx
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from 'react'
 
 const AddIndex = () => {
-
+  const [isConfirm, setIsConfirm] = useState(false);
+  const [isFinished, setIsFinished] = useState(false);
+  const [collectCount, setCollectCount] = useState(0);
   const [questions, setQuestions] = useState([]);  
 
   const makeQuestions = () => {
-    setQuestions([questions, 'new q'])
+    let newQuestions = []
+    for(let i=0; i < 9; i++){
+      let newQuestion = [i, i+1, i+2]
+      newQuestions = [...newQuestions, newQuestion]
+
+    }
+    setQuestions(newQuestions)
   }
   useEffect(() => makeQuestions(), [])
   return (
     <div>
+      {isFinished && <div>結果</div>}
       <div>{questions.map((q, idx) => <p key={idx}>{q}</p>)}</div>
       <div>□ = <input type="number" /></div>
 
